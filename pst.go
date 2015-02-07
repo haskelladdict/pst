53ab473126fcafe01839105be3c78cbffdc563ee
Markus Dittrich <haskelladdict@gmail.com>
2015-02-07 10:37:31 -0500

    Renamed some command line flags to simplify their use.
    The previous flags just seemed unintuitive somehow.

--- pst.go
+++ pst.go
@@ -40,34 +40,35 @@ var (
 type parseSpec []int
 
 func init() {
-	flag.StringVar(&spec.input, "e", "",
-		`specify the input columns to extract.
+	flag.StringVar(&spec.input, "i", "",
+		`specify the input columns to extract. This flag is optional.
      The spec format is "<column list file1>|<column list file2>|..."
      where each column specifier is of the form col_i,col_j,col_k-col_n, ....
      If the number of specifiers is less than the number of files, the last
      specifier i will be applied to files i through N, where N is the total
-     number of files provided.`)
+     number of files provided. If this flag is not provided all input columns
+     will be extracted.`)
 	flag.BoolVar(&computeStats, "c", false,
 		`compute statistics across column values in each output row.
      Please note that each value in the output has to be convertible into a float
      for this to work. Currently the mean and standard deviation are computed.`)
-	flag.StringVar(&spec.inputSep, "i", "",
+	flag.StringVar(&spec.inputSep, "s", "",
 		`column separator for input files. The default separator is whitespace.`)
-	flag.StringVar(&spec.outputSep, "o", " ",
+	flag.StringVar(&spec.outputSep, "t", " ",
 		`column separator for output files. The default separator is a single space.`)
 	flag.BoolVar(&showHelp, "h", false, "show basic usage info")
-	flag.StringVar(&spec.output, "p", "",
-		`specify the order in which to paste the output columns.
+	flag.StringVar(&spec.output, "o", "",
+		`specify the order in which to print the output columns. This flag is optional.
      The spec format is "i,j,k-l,m,..", where 0 < i,j,k,l,m, ... < numCol, and
      numCol is the total number of columns extracted from the input files.
-          Columns can be specified multiple times and ranges are accepted. If this
+     Columns can be specified multiple times and ranges are accepted. If this
      option is not provided the columns are pasted in the order in which they
      are extracted.`)
 	flag.StringVar(&spec.rows, "r", "",
-		`specify which rows to process and output.
-     This flag is optional. If not specified all rows will be output. Rows can
-     be specified by a comma separated list of row IDs or row ID ranges. E.g.,
-     "1,2,4-8,22" will process rows 1, 2, 4, 5, 7, 22.`)
+		`specify which rows to process and output. This flag is optional.
+     If not specified all rows will be output. Rows can be specified by a comma
+     separated list of row IDs or row ID ranges. E.g., "1,2,4-8,22" will process
+     rows 1, 2, 4, 5, 7, 22.`)
 	flag.IntVar(&numThreads, "n", 1, "number of threads (default: 1)")
 }
 
@@ -562,25 +563,25 @@ const exampleText = `Notes:
 
 Examples:
 
-    pst -e "0,1" file1 file2 file3 > outfile
+    pst -i "0,1" file1 file2 file3 > outfile
 
     This command selects columns 0 and 1 from each of file1, file2, and file3
    	and outputs them to outfile (which thus contains 6 columns).
 
 
-    pst -e "0,1|3" file1 file2 file3 > outfile
+    pst -i "0,1|3" file1 file2 file3 > outfile
 
     This invocation selects columns 0 and 1 from file1, and column 3 from file2
     and file3. outfile contains 4 columns.
 
 
-    pst -e "0,1|3|4-5" file1 file2 file3 > outfile
+    pst -i "0,1|3|4-5" file1 file2 file3 > outfile
 
     This command selects column 0 and 1 from file1, column 3 from file2, and
     columns 4 and 5 from file 3. outfile contains 5 columns.
 
 
-    pst -o "," -i ";" -e "0,1|3|4-5" file1 file2 file3 > outfile
+    pst -t "," -s ";" -i "0,1|3|4-5" file1 file2 file3 > outfile
 
     This command splits the input files into columns with ';' as
     separator. It selects column 0 and 1 from file1, column 3 from file2, and
@@ -588,7 +589,7 @@ Examples:
     by ','.
 
 
-    pst -c -o "," -i ";" -e "0,1|3|4-5" file1 file2 file3 > outfile
+    pst -c -t "," -s ";" -i "0,1|3|4-5" file1 file2 file3 > outfile
 
     Same as above but instead of outputting 5 columns, it computes and prints
     for each row the mean and variance across each 5 columns. Please note that
